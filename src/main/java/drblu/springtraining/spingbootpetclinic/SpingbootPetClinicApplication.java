28e4d24ee24ab120ddc4f4cfb0982c9b169503f9
Yann <yannyavo@gmail.com>
2019-07-14 23:22:48 +0200

    First commit

---
+++ src/main/java/drblu/springtraining/spingbootpetclinic/SpingbootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package drblu.springtraining.spingbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/drblu/springtraining/spingbootpetclinic/SpingbootPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package drblu.springtraining.spingbootpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingbootPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
