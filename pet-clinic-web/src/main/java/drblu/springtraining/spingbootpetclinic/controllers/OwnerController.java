65b383540e24fbf1ab5e4fc82df6fb8ffad3c79f
yayavo <yann.yavo@sogeti.com>
2019-07-23 14:03:16 +0200

    Edit model properties from owner to owners

--- pet-clinic-web/src/main/java/drblu/springtraining/spingbootpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/drblu/springtraining/spingbootpetclinic/controllers/OwnerController.java
@@ -17,7 +17,7 @@ public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
-        model.addAttribute("owner", ownerService.findAll());
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
