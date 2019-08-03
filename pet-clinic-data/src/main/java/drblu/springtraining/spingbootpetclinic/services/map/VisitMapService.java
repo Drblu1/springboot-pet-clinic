bce450e900b26f21e094859e4f6e3c411237826f
Yann <yannyavo@gmail.com>
2019-08-03 12:30:39 +0200

    add visit service and implementation

---
+++ pet-clinic-data/src/main/java/drblu/springtraining/spingbootpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package drblu.springtraining.spingbootpetclinic.services.map;
+
+import drblu.springtraining.spingbootpetclinic.model.Visit;
+import drblu.springtraining.spingbootpetclinic.services.springdatajpa.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit entity) {
+        super.delete(entity);
+    }
+
+    @Override
+    public Visit save(Visit entity) {
+        if(entity.getPet() == null || entity.getPet().getOwner() == null || entity.getPet().getId() == null
+            || entity.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+
+        }
+        return super.save(entity);
+    }
+}
---
+++ pet-clinic-data/src/main/java/drblu/springtraining/spingbootpetclinic/services/springdatajpa/VisitService.java
@@ -0,0 +1,7 @@
+package drblu.springtraining.spingbootpetclinic.services.springdatajpa;
+
+import drblu.springtraining.spingbootpetclinic.model.Visit;
+import drblu.springtraining.spingbootpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
