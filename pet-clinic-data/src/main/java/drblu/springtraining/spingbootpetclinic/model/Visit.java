cc120a1aa2f2ef763099b16f33429348b7374442
Yann <yannyavo@gmail.com>
2019-08-03 11:16:53 +0200

    Visit entity mapping done

--- pet-clinic-data/src/main/java/drblu/springtraining/spingbootpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/drblu/springtraining/spingbootpetclinic/model/Visit.java
@@ -1,12 +1,21 @@
 package drblu.springtraining.spingbootpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
-    private Pet Pet;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
@@ -24,11 +33,11 @@ public class Visit extends BaseEntity{
         this.description = description;
     }
 
-    public drblu.springtraining.spingbootpetclinic.model.Pet getPet() {
-        return Pet;
+    public Pet getPet() {
+        return pet;
     }
 
-    public void setPet(drblu.springtraining.spingbootpetclinic.model.Pet pet) {
-        Pet = pet;
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 }
